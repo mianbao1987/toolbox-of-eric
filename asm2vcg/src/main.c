b58b697c6905d8fca1245978e0c5876af84292ac
joefoxreal <joefoxreal@d4dabd8c-ea04-11de-9d05-050bbbc75a16>
2010-01-07 05:53:56 +0000

    bump version to 0.2

--- asm2vcg/src/main.c
+++ asm2vcg/src/main.c
@@ -4,7 +4,7 @@
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
+   the Free Software Foundation, either version 2 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -34,7 +34,7 @@ FILE *fin;
 FILE *fout;
 
 char *program_name;
-char *program_version = "0.1";
+char *program_version = "0.2";
 
 char *current_fun_name;
 int current_bb_num = -1;
