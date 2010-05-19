3156c9150ca8348d63c603ae5f82fb283211b7c6
joefoxreal <joefoxreal@d4dabd8c-ea04-11de-9d05-050bbbc75a16>
2010-05-19 06:22:33 +0000

    update vcg.c

--- tree2vcg/src/vcg.c
+++ tree2vcg/src/vcg.c
@@ -90,6 +90,7 @@ cfg_to_vcg (void)
           else
             gdl_set_node_label (node, bb->text);
 
+          gdl_set_node_vertical_order (node, bb->max_distance);
           gdl_add_node (bb_graph, node);
         }
 
